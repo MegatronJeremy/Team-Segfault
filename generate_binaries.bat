b59977ee2e1fe3b36fb94773a70fdd2e476a5be0
Vuk Djordjevic <vukd10@gmail.com>
2024-09-23 10:43:09 +0200

    Pause added to generate_binaries.bat

--- generate_binaries.bat
+++ generate_binaries.bat
@@ -1,9 +1,4 @@
 @echo off
-REM Check if PowerShell script exists
-if not exist generate_binaries.ps1 (
-    echo generate_binaries.ps1 does not exist.
-    exit /b 1
-)
 
 REM Run the PowerShell script
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File generate_binaries.ps1
@@ -11,7 +6,12 @@ powershell.exe -NoProfile -ExecutionPolicy Bypass -File generate_binaries.ps1
 REM Check if the PowerShell script ran successfully
 if %errorlevel% neq 0 (
     echo PowerShell script generate_binaries.ps1 failed.
+    
+    pause
     exit /b 1
 )
 
 echo PowerShell script generate_binaries.ps1 ran successfully.
+
+REM Keep the window open
+pause
